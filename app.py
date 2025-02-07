fd1887ec8dda023fcd28de92b73730d412265bc6
Kai Barker <kfb36@cornell.edu>
2025-02-06 23:22:36 -0500

    more movement

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import *
+from flask import Flask, render_template
 from whitenoise import WhiteNoise
 
-
 app = Flask(__name__)
-app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/", prefix="static/", index_file="index.html", autorefresh=True)
 
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/", prefix="static/", autorefresh=True)
+
+@app.route("/")
+def home():
+    return render_template("index.html")  
 
 if __name__ == "__main__":
-  app.run(threaded=True, port=9000)
+    app.run(threaded=True, port=9000)
